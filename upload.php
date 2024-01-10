b5821a1650e35dc193fb00c492f6598afcd78b27
JonathanClv <151640974+JonathanClv@users.noreply.github.com>
2024-01-10 15:40:39 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,2 +1,15 @@
-<a href="index.html">home</a>
-<a href="portfolio.html">portfolio documenten</a>
+<?php
+$uploadDir = 'uploads/';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $bestandsnaam = $_FILES['bestand']['name'];
+    $bestandslocatie = $uploadDir . $bestandsnaam;
+
+    if (move_uploaded_file($_FILES['bestand']['tmp_name'], $bestandslocatie)) {
+        header("Location: index.html");
+        exit();
+    } else {
+        echo "Fout bij het uploaden van het bestand.";
+    }
+}
+?>
